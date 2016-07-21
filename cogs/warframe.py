24f7fcf4e263221d47a9ef66fdb1abfc9d9b58d4
Alvin On (Chocomunk) <alvin.zhou.on@gmail.com>
2016-07-20 23:33:22 -0700

    Added search for void trader items. Fixed spacing for table messages. Added response when listing on a server with no filters. - Chocomunk

--- cogs/warframe.py
+++ cogs/warframe.py
@@ -11,12 +11,45 @@ except:
 	soupAvailable = False
 import aiohttp
 
+class SearchFilter:
+	
+	def __init__(self, expression: str, custom_filters):
+		self.expr = expression
+		s_clear = False
+		while(not s_clear):
+			for f in custom_filters:
+				self.expr = self.expr.replace(f, custom_filters[f])
+			s_clear = True
+			for f in custom_filters:
+				s_clear = s_clear & (f not in self.expr)
+
+		self.filters = [i.rstrip().lstrip() for i in re.split('[()&|!]', self.expr) if i.strip()!='']
+		for i in self.filters:
+			self.expr = self.expr.replace(i, "{:}")
+		self.expr = self.expr.strip()
+		self.expr = self.expr.replace('!{:}', '( not {:})')
+				
+	def passes(self, inp: list):
+		if(self.expr == ""):
+			return True
+		else:
+			comp = []
+			for f in self.filters:
+				res = False
+				for i in inp:
+					res = res or (f.lower() in i.lower())
+					if res:
+						break
+				comp.append(res)
+			result = eval(self.expr.format(*comp))
+			return result
+
 class PrimeList:
-	"""My custom cog that does stuff!"""
+	"""Provides information on prime items"""
 
 	def __init__(self, bot):
 		self.bot = bot
-		self.filters = fileIO("data/warframe/filters.json", "load")
+		self.filters = fileIO("data/warframe/prime_filters.json", "load")
 
 	@commands.group(name="prime", pass_context=True)
 	async def prime(self, ctx):
@@ -88,7 +121,7 @@ class PrimeList:
 		if filter not in self.filters[server.id]:
 			val = "".join(to_search)
 			self.filters[server.id][filter] = val
-			fileIO("data/warframe/filters.json", "save", self.filters)
+			fileIO("data/warframe/prime_filters.json", "save", self.filters)
 			await self.bot.say("Custom filter '{0}' added."
 							   "It will correspond to the filter: '{1}'".format(filter, val))
 		else:
@@ -101,7 +134,7 @@ class PrimeList:
 		server = ctx.message.server
 		if server.id in self.filters:
 			self.filters[server.id].pop(filter, None)
-			fileIO("data/warframe/filters.json", "save", self.filters)
+			fileIO("data/warframe/prime_filters.json", "save", self.filters)
 		await self.bot.say("Deleted custom filter '{0}'".format(filter))
 
 	@prime_filters.command(name="show", pass_context=True)
@@ -119,20 +152,25 @@ class PrimeList:
 		"""Lists all custom filters"""
 		server = ctx.message.server
 		if server.id in self.filters:
-			message = "```Custom Filter list:\n"
-			for f in sorted(self.filters[server.id]):
-				if len(message) + len(f) + 3 > 2000:
+			if len(self.filters[server.id]) > 0:
+				message = "```Custom Filter list:\n"
+				for f in sorted(self.filters[server.id]):
+					if len(message) + len(f) + 3 > 2000:
+						await self.bot.say(message)
+						message = "```\n"
+					message += "\t{0:<15s}\t=\t{1:<}\n".format(f, self.filters[server.id][f])
+				if len(message) > 4:
+					message += "```"
 					await self.bot.say(message)
-					message = "```\n"
-				message += "\t{0:<15s}\t=\t{1:<}\n".format(f, self.filters[server.id][f])
-			if len(message) > 4:
-				message += "```"
-				await self.bot.say(message)
+			else:
+				await self.bot.say("This server does not have any filters")
+		else:
+			await self.bot.say("This server does not have any filters")
 
 	def _parse_table(self, filter, so):
 		rows = so.find(id="mw-customcollapsible-ducatsprices").find('table').find_all('tr')
 		set = []
-		set.append(["{:^25s}|{:^35s}|{:^4s}|{:^4s}".format("PART","DROP LOCATION","BP","CRAFT")])
+		set.append(["{:^25s}|{:^35s}|{:^2s}|{:^5s}".format("PART","DROP LOCATION","BP","CRAFT")])
 		temp = []
 		del rows[0]
 		for row in rows:
@@ -151,43 +189,178 @@ class PrimeList:
 				if len(temp) == 20:
 					set.append(list(temp))
 					temp = []
-				temp.append("{:^25s}|{:^35s}|{:^4s}|{:^4s}".format(name, location, blueprint_value, crafted_value))
+				temp.append("{:^25s}|{:^35s}|{:^2s}|{:^5s}".format(name, location, blueprint_value, crafted_value))
+		set.append(list(temp))
+		return set
+			
+class VoidTrader:
+	"""Provides information on Baro Ki'Teer"""
+
+	def __init__(self, bot):
+		self.bot = bot
+		self.filters = fileIO("data/warframe/trader_filters.json", "load")
+
+	@commands.group(name="voidtrader", pass_context="True")
+	async def voidtrader(self, ctx):
+		"""Provides information on Baro Ki'Teer"""
+		if ctx.invoked_subcommand is None:
+			await send_cmd_help(ctx)
+			return
+
+	# @voidtrader.command(name="update")
+	async def _trader_update(self):
+		"""Provides update on Baro Ki'Teer"""
+		await self.bot.say("Gathering Update Data, Please wait ...")
+
+		url = "https://deathsnacks.com/wf/"
+		async with aiohttp.get(url) as response:
+			so = BeautifulSoup(await response.text(), "html.parser")
+		info = so.find(id="vtaccordion").find_all(True)
+		print('\n')
+		print(info)
+		# info = so.find('a', attrs={'class': 'toggler-vt list-group-item'})
+		# info = info.find('span')
+		info = info.find_all('span')
+		print(info)
+
+		await self.bot.say("{0}: {1}".format(info[0].get_text().rstrip(),info[1].get_text().rstrip()))
+		# try:
+			
+		# except:
+		# 	await self.bot.say("Couldn't load item info")
+
+	@voidtrader.command(name="list")
+	async def _trader_list(self):
+		"""Lists info for all trader items"""
+		try:
+			await self.bot.say("Gathering Item Data, Please wait ...")
+
+			url = "http://warframe.wikia.com/wiki/Baro_Ki'Teer/Trades"
+			async with aiohttp.get(url) as response:
+				so = BeautifulSoup(await response.text(), "html.parser")
+			for s in self._parse_table(SearchFilter("",[]), so):
+				output = "```" + "\n".join(s) + "```"
+				await self.bot.say(output)
+		except:
+			await self.bot.say("Couldn't load item info")
+
+	@voidtrader.command(name="search", pass_context=True)
+	async def _trader_search(self, ctx, *filter):
+		"""Searches for trader items with a filter"""
+		server = ctx.message.server
+		if(len(filter) == 0):
+			await send_cmd_help(ctx)
+			return
+		else:
+			try:
+				await self.bot.say("Gathering Item Data, Please wait ...")
+
+				if server.id in self.filters:
+					filt = SearchFilter("".join(filter), self.filters[server.id])
+				else:
+					filt = SearchFilter("".join(filter), [])
+
+				url = "http://warframe.wikia.com/wiki/Baro_Ki'Teer/Trades"
+				async with aiohttp.get(url) as response:
+					so = BeautifulSoup(await response.text(), "html.parser")
+				for s in self._parse_table(filt, so):
+					output = "```" + "\n".join(s) + "```"
+					await self.bot.say(output)
+			except:
+				await self.bot.say("Couldn't load item info")
+
+	@voidtrader.group(name="filters", pass_context=True)
+	async def trader_filters(self, ctx):
+		"""Options for Search Filters"""
+		if ctx.invoked_subcommand is None:
+			await send_cmd_help(ctx)
+			return
+		else:
+			sub_cm = ctx.invoked_subcommand.name
+			if ctx.command.name in sub_cm:
+				sub_cm = sub_cm.strip(ctx.command.name)
+			if sub_cm is "":
+				await send_cmd_help(ctx)
+				return
+		
+	@trader_filters.command(name="add", pass_context=True)
+	async def _trader_filter_add(self, ctx, filter: str, *to_search):
+		"""Adds a custom filter"""
+		server = ctx.message.server
+		if server.id not in self.filters:
+			self.filters[server.id] = {}
+		if filter not in self.filters[server.id]:
+			val = "".join(to_search)
+			self.filters[server.id][filter] = val
+			fileIO("data/warframe/trader_filters.json", "save", self.filters)
+			await self.bot.say("Custom filter '{0}' added."
+							   "It will correspond to the filter: '{1}'".format(filter, val))
+		else:
+			await self.bot.say("'{0}' already exists as a custom filter."
+							   "'{0}' filters for: '{1}'".format(filter,self.filters[server.id][filter]))
+
+	@trader_filters.command(name="del", pass_context=True)
+	async def _trader_filter_del(self, ctx, filter: str):
+		"""Deletes a custom filter"""
+		server = ctx.message.server
+		if server.id in self.filters:
+			self.filters[server.id].pop(filter, None)
+			fileIO("data/warframe/trader_filters.json", "save", self.filters)
+		await self.bot.say("Deleted custom filter '{0}'".format(filter))
+
+	@trader_filters.command(name="show", pass_context=True)
+	async def _trader_filter_show(self, ctx, filter: str):
+		"""Shows the what [filter] filters"""
+		server = ctx.message.server
+		if server.id in self.filters:
+			if filter in self.filters[server.id]:
+				await self.bot.say("'{0}' filters for '{1}".format(filter,self.filters[server.id][filter]))
+			else:
+				await self.bot.say("Custom filter '{0}' does not exist on this server".format(filter))
+
+	@trader_filters.command(name="list", pass_context=True)
+	async def _trader_filter_list(self, ctx):
+		"""Lists all custom filters"""
+		server = ctx.message.server
+		if server.id in self.filters:
+			if len(self.filters[server.id]) > 0:
+				message = "```Custom Filter list:\n"
+				for f in sorted(self.filters[server.id]):
+					if len(message) + len(f) + 3 > 2000:
+						await self.bot.say(message)
+						message = "```\n"
+					message += "\t{0:<15s}\t=\t{1:<}\n".format(f, self.filters[server.id][f])
+				if len(message) > 4:
+					message += "```"
+					await self.bot.say(message)
+			else:
+				await self.bot.say("This server does not have any filters")
+		else:
+			await self.bot.say("This server does not have any filters")
+
+	def _parse_table(self, filter, so):
+		rows = so.find(id="mw-customcollapsible-itemsale").find('table').find_all('tr')
+		set = []
+		set.append(["{:^40s}|{:^10s}|{:^8s}".format("ITEM","CREDITS","DUCATS")])
+		temp = []
+		del rows[0]
+		for row in rows:
+			cells = row.find_all('td')
+			descriptors = cells[0].find_all('a')
+			prices = cells[1].find_all('span')
+
+			name = descriptors[1].get_text().rstrip()
+			credits = "C"+prices[0].get_text().rstrip()
+			ducats = "D"+prices[1].get_text().rstrip()
+
+			if filter.passes([name,credits,ducats]):
+				if len(temp) == 20:
+					set.append(list(temp))
+					temp = []
+				temp.append("{:^40s}|{:^10s}|{:^8s}".format(name, credits, ducats))
 		set.append(list(temp))
 		return set
 
-class SearchFilter:
-	
-	def __init__(self, expression: str, custom_filters):
-		self.expr = expression
-		s_clear = False
-		while(not s_clear):
-			for f in custom_filters:
-				self.expr = self.expr.replace(f, custom_filters[f])
-			s_clear = True
-			for f in custom_filters:
-				s_clear = s_clear & (f not in self.expr)
-
-		self.filters = [i.rstrip().lstrip() for i in re.split('[()&|!]', self.expr) if i.strip()!='']
-		for i in self.filters:
-			self.expr = self.expr.replace(i, "{:}")
-		self.expr = self.expr.strip()
-		self.expr = self.expr.replace('!{:}', '( not {:})')
-				
-	def passes(self, inp: list):
-		if(self.expr == ""):
-			return True
-		else:
-			comp = []
-			for f in self.filters:
-				res = False
-				for i in inp:
-					res = res or (f.lower() in i.lower())
-					if res:
-						break
-				comp.append(res)
-			result = eval(self.expr.format(*comp))
-			return result
-			
 def check_folder():
 	if not os.path.exists("data/warframe"):
 		print("Creating data/warframe folder...")
@@ -195,17 +368,20 @@ def check_folder():
 
 
 def check_files():
-	filters = {}
+	default = {}
 
-	f = "data/warframe/filters.json"
-	if not fileIO(f, "check"):
-		print("Creating default warframes's filters.json...")
-		fileIO(f, "save", filters)
+	files = ["prime_filters", "trader_filters"]
+	for f in files:
+		loc = "data/warframe/{}.json".format(f)
+		if not fileIO(loc, "check"):
+			print("Creating default warframes's {}.json...".format(f))
+			fileIO(loc, "save", default)
 
 def setup(bot):
 	check_folder()
 	check_files()
 	if soupAvailable:
 		bot.add_cog(PrimeList(bot))
+		bot.add_cog(VoidTrader(bot))
 	else:
 		raise RuntimeError("You need to run `pip3 install beautifulsoup4`")
